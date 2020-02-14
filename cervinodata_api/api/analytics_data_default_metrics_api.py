e2d4c3bde63aa8e9c876eeef0bcd5bf7282de647
jaccodrenth_cm <_3Nwn7cxgP-n>
2020-02-14 13:25:48 +0100

    automated update

--- cervinodata_api/api/analytics_data_default_metrics_api.py
+++ cervinodata_api/api/analytics_data_default_metrics_api.py
@@ -277,6 +277,126 @@ class AnalyticsDataDefaultMetricsApi(object):
             _request_timeout=local_var_params.get('_request_timeout'),
             collection_formats=collection_formats)
 
+    def get_analytics_report_per_device_per_channel_group_per_day(self, organisation_uuid, **kwargs):  # noqa: E501
+        """Return analytics report per device per channel group per day by organisation  # noqa: E501
+
+        Analytics report per device per channel group per day by organisation  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_analytics_report_per_device_per_channel_group_per_day(organisation_uuid, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool: execute request asynchronously
+        :param str organisation_uuid: Organisation uuid (required)
+        :param date from_date: From date
+        :param str date_format: Outputted date format
+        :param str format: Output format (use csv for large result sets)
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: str
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+        kwargs['_return_http_data_only'] = True
+        return self.get_analytics_report_per_device_per_channel_group_per_day_with_http_info(organisation_uuid, **kwargs)  # noqa: E501
+
+    def get_analytics_report_per_device_per_channel_group_per_day_with_http_info(self, organisation_uuid, **kwargs):  # noqa: E501
+        """Return analytics report per device per channel group per day by organisation  # noqa: E501
+
+        Analytics report per device per channel group per day by organisation  # noqa: E501
+        This method makes a synchronous HTTP request by default. To make an
+        asynchronous HTTP request, please pass async_req=True
+        >>> thread = api.get_analytics_report_per_device_per_channel_group_per_day_with_http_info(organisation_uuid, async_req=True)
+        >>> result = thread.get()
+
+        :param async_req bool: execute request asynchronously
+        :param str organisation_uuid: Organisation uuid (required)
+        :param date from_date: From date
+        :param str date_format: Outputted date format
+        :param str format: Output format (use csv for large result sets)
+        :param _return_http_data_only: response data without head status code
+                                       and headers
+        :param _preload_content: if False, the urllib3.HTTPResponse object will
+                                 be returned without reading/decoding response
+                                 data. Default is True.
+        :param _request_timeout: timeout setting for this request. If one
+                                 number provided, it will be total request
+                                 timeout. It can also be a pair (tuple) of
+                                 (connection, read) timeouts.
+        :return: tuple(str, status_code(int), headers(HTTPHeaderDict))
+                 If the method is called asynchronously,
+                 returns the request thread.
+        """
+
+        local_var_params = locals()
+
+        all_params = ['organisation_uuid', 'from_date', 'date_format', 'format']  # noqa: E501
+        all_params.append('async_req')
+        all_params.append('_return_http_data_only')
+        all_params.append('_preload_content')
+        all_params.append('_request_timeout')
+
+        for key, val in six.iteritems(local_var_params['kwargs']):
+            if key not in all_params:
+                raise ApiTypeError(
+                    "Got an unexpected keyword argument '%s'"
+                    " to method get_analytics_report_per_device_per_channel_group_per_day" % key
+                )
+            local_var_params[key] = val
+        del local_var_params['kwargs']
+        # verify the required parameter 'organisation_uuid' is set
+        if self.api_client.client_side_validation and ('organisation_uuid' not in local_var_params or  # noqa: E501
+                                                        local_var_params['organisation_uuid'] is None):  # noqa: E501
+            raise ApiValueError("Missing the required parameter `organisation_uuid` when calling `get_analytics_report_per_device_per_channel_group_per_day`")  # noqa: E501
+
+        collection_formats = {}
+
+        path_params = {}
+        if 'organisation_uuid' in local_var_params:
+            path_params['organisationUuid'] = local_var_params['organisation_uuid']  # noqa: E501
+
+        query_params = []
+        if 'from_date' in local_var_params and local_var_params['from_date'] is not None:  # noqa: E501
+            query_params.append(('from_date', local_var_params['from_date']))  # noqa: E501
+        if 'date_format' in local_var_params and local_var_params['date_format'] is not None:  # noqa: E501
+            query_params.append(('date_format', local_var_params['date_format']))  # noqa: E501
+        if 'format' in local_var_params and local_var_params['format'] is not None:  # noqa: E501
+            query_params.append(('format', local_var_params['format']))  # noqa: E501
+
+        header_params = {}
+
+        form_params = []
+        local_var_files = {}
+
+        body_params = None
+        # HTTP header `Accept`
+        header_params['Accept'] = self.api_client.select_header_accept(
+            ['text/csv', 'application/json'])  # noqa: E501
+
+        # Authentication setting
+        auth_settings = ['bearerAuth']  # noqa: E501
+
+        return self.api_client.call_api(
+            '/data/analytics-report-per-device-per-channel-group-per-day/{organisationUuid}', 'GET',
+            path_params,
+            query_params,
+            header_params,
+            body=body_params,
+            post_params=form_params,
+            files=local_var_files,
+            response_type='str',  # noqa: E501
+            auth_settings=auth_settings,
+            async_req=local_var_params.get('async_req'),
+            _return_http_data_only=local_var_params.get('_return_http_data_only'),  # noqa: E501
+            _preload_content=local_var_params.get('_preload_content', True),
+            _request_timeout=local_var_params.get('_request_timeout'),
+            collection_formats=collection_formats)
+
     def get_analytics_report_per_device_per_day(self, organisation_uuid, **kwargs):  # noqa: E501
         """Return analytics report per device per day by organisation  # noqa: E501
 
--- docs/AnalyticsDataDefaultMetricsApi.md
+++ docs/AnalyticsDataDefaultMetricsApi.md
@@ -6,6 +6,7 @@ Method | HTTP request | Description
 ------------- | ------------- | -------------
 [**get_analytics_report_per_campaign_per_day**](AnalyticsDataDefaultMetricsApi.md#get_analytics_report_per_campaign_per_day) | **GET** /data/analytics-report-per-campaign-per-day/{organisationUuid} | Return analytics report per campaign per day by organisation
 [**get_analytics_report_per_channel_group_per_day**](AnalyticsDataDefaultMetricsApi.md#get_analytics_report_per_channel_group_per_day) | **GET** /data/analytics-report-per-channel-group-per-day/{organisationUuid} | Return analytics report per channel group per day by organisation
+[**get_analytics_report_per_device_per_channel_group_per_day**](AnalyticsDataDefaultMetricsApi.md#get_analytics_report_per_device_per_channel_group_per_day) | **GET** /data/analytics-report-per-device-per-channel-group-per-day/{organisationUuid} | Return analytics report per device per channel group per day by organisation
 [**get_analytics_report_per_device_per_day**](AnalyticsDataDefaultMetricsApi.md#get_analytics_report_per_device_per_day) | **GET** /data/analytics-report-per-device-per-day/{organisationUuid} | Return analytics report per device per day by organisation
 [**get_analytics_report_per_source_medium_per_day**](AnalyticsDataDefaultMetricsApi.md#get_analytics_report_per_source_medium_per_day) | **GET** /data/analytics-report-per-source-medium-per-day/{organisationUuid} | Return analytics report per source medium per day by organisation
 
@@ -113,6 +114,74 @@ try:
     pprint(api_response)
 except ApiException as e:
     print("Exception when calling AnalyticsDataDefaultMetricsApi->get_analytics_report_per_channel_group_per_day: %s\n" % e)
+```
+
+### Parameters
+
+Name | Type | Description  | Notes
+------------- | ------------- | ------------- | -------------
+ **organisation_uuid** | **str**| Organisation uuid | 
+ **from_date** | **date**| From date | [optional] 
+ **date_format** | **str**| Outputted date format | [optional] 
+ **format** | **str**| Output format (use csv for large result sets) | [optional] 
+
+### Return type
+
+**str**
+
+### Authorization
+
+[bearerAuth](../README.md#bearerAuth)
+
+### HTTP request headers
+
+ - **Content-Type**: Not defined
+ - **Accept**: text/csv, application/json
+
+### HTTP response details
+| Status code | Description | Response headers |
+|-------------|-------------|------------------|
+**200** | successful operation |  -  |
+**400** | Invalid Organisation uuid supplied |  -  |
+**404** | Organisation uuid not found |  -  |
+
+[[Back to top]](#) [[Back to API list]](../README.md#documentation-for-api-endpoints) [[Back to Model list]](../README.md#documentation-for-models) [[Back to README]](../README.md)
+
+# **get_analytics_report_per_device_per_channel_group_per_day**
+> str get_analytics_report_per_device_per_channel_group_per_day(organisation_uuid, from_date=from_date, date_format=date_format, format=format)
+
+Return analytics report per device per channel group per day by organisation
+
+Analytics report per device per channel group per day by organisation
+
+### Example
+
+* Bearer Authentication (bearerAuth):
+```python
+from __future__ import print_function
+import time
+import cervinodata_api
+from cervinodata_api.rest import ApiException
+from pprint import pprint
+configuration = cervinodata_api.Configuration()
+# Configure Bearer authorization: bearerAuth
+configuration.access_token = 'YOUR_BEARER_TOKEN'
+
+# Defining host is optional and default to https://app.cervinodata.com/api/v1
+configuration.host = "https://app.cervinodata.com/api/v1"
+# Create an instance of the API class
+api_instance = cervinodata_api.AnalyticsDataDefaultMetricsApi(cervinodata_api.ApiClient(configuration))
+organisation_uuid = 'organisation_uuid_example' # str | Organisation uuid
+from_date = '2013-10-20' # date | From date (optional)
+date_format = 'date_format_example' # str | Outputted date format (optional)
+format = 'format_example' # str | Output format (use csv for large result sets) (optional)
+
+try:
+    # Return analytics report per device per channel group per day by organisation
+    api_response = api_instance.get_analytics_report_per_device_per_channel_group_per_day(organisation_uuid, from_date=from_date, date_format=date_format, format=format)
+    pprint(api_response)
+except ApiException as e:
+    print("Exception when calling AnalyticsDataDefaultMetricsApi->get_analytics_report_per_device_per_channel_group_per_day: %s\n" % e)
 ```
 
 ### Parameters
